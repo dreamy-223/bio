c5948e28b59c6c00c3828c12cca25375f31bf103
Dreamy <danieljay223@gmail.com>
2026-04-24 02:55:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let button1 = document.getElementById("button1");
+let button2 = document.getElementById("button2");
+let button3 = document.getElementById("button3");
+
+button1.onclick = function() {
+    window.open("https://discord.gg/CPbsmFV49Y");
+}
+
+button2.onclick = function() {
+    window.open("https://www.tiktok.com/@daniel_hicks223?lang=th-TH");
+}
+
+button3.onclick = function() {
+    window.open("https://www.instagram.com/danie223l_jay");
+}
