b9c7e42b716485b014110ae1224b60bfc35f1585
Dreamy <danieljay223@gmail.com>
2026-04-24 23:48:53 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let button2 = document.getElementById("button2");
 let button3 = document.getElementById("button3");
 let button4 = document.getElementById("button4");
 let button5 = document.getElementById("button5");
+let button6 = document.getElementById("button6");
 
 button1.onclick = function() {
     window.open("https://discord.gg/CPbsmFV49Y");
@@ -22,4 +23,8 @@ button4.onclick = function() {
 
 button5.onclick = function() {
     window.open("https://line.me/ti/p/K01IcKWMO8");
+}
+
+button6.onclick = function() {
+    window.open("hhttps://www.roblox.com/share?code=5e486c946d23584d950d2f19501105c6&type=Profile&source=ProfileShare&stamp=1777048401409");
 }
