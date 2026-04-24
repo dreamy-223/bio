4110a669d846a7f80c88e7497e83684c12ff7fe6
Dreamy <danieljay223@gmail.com>
2026-04-24 21:03:10 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let button1 = document.getElementById("button1");
 let button2 = document.getElementById("button2");
 let button3 = document.getElementById("button3");
+let button4 = document.getElementById("button4");
+let button5 = document.getElementById("button5");
 
 button1.onclick = function() {
     window.open("https://discord.gg/CPbsmFV49Y");
@@ -12,4 +14,12 @@ button2.onclick = function() {
 
 button3.onclick = function() {
     window.open("https://www.instagram.com/danie223l_jay");
+}
+
+button4.onclick = function() {
+    window.open("https://web.facebook.com/danieljay.hicks.7");
+}
+
+button5.onclick = function() {
+    window.open("https://line.me/ti/p/K01IcKWMO8");
 }
